f1374392b5c519651825776909af967f282ff914
yossefezzat <45318576+yossefezzat@users.noreply.github.com>
2019-12-21 13:20:09 +0200

    arch1_NN_kFlud

--- NN.py
+++ NN.py
@@ -3,51 +3,53 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-
-
-train_data , validation_data ,test_data = final_data()
-
 image_size = WIDTH*HEIGHT
-X_train = list()
-Y_train = list()
-for i in range(len(train_data)):
-    X_train.append(train_data[i][:900])
-    Y_train.append(train_data[i][900:])
 
 
+def extract_train_valid_test():
+    train_data , validation_data ,test_data = final_data()
 
-X_validation = list()
-Y_validation = list()
-
-for i in range(len(validation_data)):
-    X_validation.append(train_data[i][:900])
-    Y_validation.append(train_data[i][900:])
-
-
-X_test = list()
-Y_test =  list()
-
-for i in range(len(test_data)):
-    X_test.append(train_data[i][:900])
-    Y_test.append(train_data[i][900:])
-
-
-
-model = keras.Sequential()
-
-
-model.add(keras.layers.Dense(220, activation='relu'))
-model.add(keras.layers.Dense(50, activation='relu'))
-model.add(keras.layers.Dense(10, activation='softmax'))
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-history = model.fit(X_train, Y_train ,epochs=10 , batch_size=64)
-
-#test_loss, test_acc = model.evaluate(X_train , Y_train)
-#print(test_loss , test_acc)
-
-predictions = model.predict(X_test)
-
+    X_train = list()
+    Y_train = list()
+    X_test = list()
+    Y_test =  list()
+    X_validation = list()
+    Y_validation = list()
     
+    for i in range(len(train_data)):
+        X_train.append(train_data[i][:900])
+        Y_train.append(train_data[i][900:])
     
-print(predictions)
+    for i in range(len(validation_data)):
+        X_validation.append(train_data[i][:900])
+        Y_validation.append(train_data[i][900:])
+    
+    for i in range(len(test_data)):
+        X_test.append(train_data[i][:900])
+        Y_test.append(train_data[i][900:])
+    return X_train, Y_train , X_validation , Y_validation , X_test , Y_test
+
+
+def k_flud_arch1_NN(num):
+    accuracies = []
+    for i in range(num):
+        X_train, Y_train , X_validation , Y_validation , X_test , Y_test = extract_train_valid_test()
+        model = keras.Sequential()
+        model.add(keras.layers.Dense(220, activation='relu'))
+        model.add(keras.layers.Dense(50, activation='relu'))
+        model.add(keras.layers.Dense(10, activation='softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        history = model.fit(X_train, Y_train ,epochs=20 , batch_size=64)
+        
+        test_loss, test_acc = model.evaluate(X_test , Y_test)
+        print(test_loss , test_acc)
+        
+        predictions = model.predict(X_test)
+        print(Y_test[0])
+        print(np.argmax(predictions[0]), predictions[0])   
+        accuracies.append(test_acc)
+    return accuracies
+
+accuracies = k_flud_arch1_NN(10)
+
+print(accuracies)
